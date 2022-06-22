9a34835dc6518aa20cf42a36a0bbf305dfa2606b
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2022-06-22 08:36:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6282261065144'],
+  ['6282261065144'],
+  ['6282261065144', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = '@FIN~botz'
+  var sticker_author = 'FIN'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
