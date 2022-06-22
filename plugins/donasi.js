2c6946f5f0bef4e3a952dfe01c56c1b05cd7e6db
Raya2399 <102332034+Raya2399@users.noreply.github.com>
2022-06-22 09:45:23 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0822-6106-5144]
+│ •  [0822-6106-5144]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
